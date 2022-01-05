031a06607391ca277967ad34c6f4e72d881da114
Vinicius Santos <viniciussantos@tokenlab.com.br>
2022-01-05 14:51:53 -0300

    base do exemplo 3

--- 3.js
+++ 3.js
@@ -1,4 +1,29 @@
-stream = require('stream')
+const {Writable, Readable, Transform, pipeline} = require('stream')
 
 
 
+const readableStream = Readable({
+  read: function() {
+
+
+  }
+
+})
+
+
+const writableStream = Writable({
+  write: function(chunk, encoding, next){
+
+
+  }
+
+
+})
+
+const transformStream = Transform({
+  transform(chunk, enconding, next){
+
+
+  }
+
+})
