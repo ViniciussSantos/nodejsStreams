095611bdc0d74e893c525cc0c07148ebcac9a47a
Vinicius Santos <viniciussantos@tokenlab.com.br>
2022-01-13 11:30:28 -0300

    events

---
+++ presentation.js
@@ -0,0 +1,17 @@
+const stream = require('stream')
+
+const readableSrc = new stream.Readable
+
+readableSrc.on('data', (data) => console.log('recebi um chunk ', data))
+readableSrc.on('error', (err)=> console.log('deu ruim! ', err))
+readableSrc.on('close',()=> console.log('fechou'))
+
+readableSrc.push('arroz1')
+readableSrc.push('arroz2')
+readableSrc.push('arroz3')
+readableSrc.emit('error', 'err')
+readableSrc.push(null)
+
+const writableSrc = new stream.Writable
+
+writableSrc.on('pipe')
