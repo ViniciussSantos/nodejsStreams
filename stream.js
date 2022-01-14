ea43eb7fe945be5403e52bbd7ece8dd955ce6e53
Vinicius Santos <viniciussantos@tokenlab.com.br>
2022-01-14 05:48:38 -0300

    finalizado, contém um pequeno bug

--- stream.js
+++ stream.js
@@ -1,17 +1,21 @@
 const { createWriteStream, createReadStream } = require('fs')
 const {Writable, Readable, Transform, pipeline, Duplex} = require('stream')
+const csvToJson = require('./csvToJson.js');
 const { promisify } = require('util')
+
+
+const toObject = csvToJson.toObject()
+const stringify = csvToJson.stringify()
 
 const asyncPipeline = promisify(pipeline)
 
 
-
 const readableStream = Readable({
   read: function() {
-    for( let i = 0; i < 10000; i++) {
+    for( let i = 0; i < 10; i++) {
       const Product = `barcode: ${Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000}, name: product-${i}\n`
       const product = { barcode: Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000, name: `product-${i}`}
-      this.push(Product)
+      this.push(JSON.stringify(product))
     }
     this.push(null)
   }
@@ -31,29 +35,33 @@ const header = Transform({
   }
 })
 
+const mapToCSV= Transform({
+  transform(chunk, enconding, callback){
 
-const readCSV = Transform({
-  transform: function(chunk, encoding, callback){
-    const data = chunk.toString()
-    const arr = data.split('\n')
-    arr.shift(0)
+    const data = JSON.parse(chunk)
 
-    callback(null,arr.toString())
+    const product = `${data.barcode},${data.name}\n`
+    callback(null, product)
+    
   }
+
+
 })
 
-/* const toJSON = Transform({
-  transform: function(chunk)
-})
- */
-
-/* asyncPipeline(
-  readableStream,
-  header,
-  createWriteStream('products.csv'),  
-) */
 
 asyncPipeline(
-  createReadStream('products.csv'),
-  readCSV,
+  readableStream,
+  mapToCSV, 
+  header,
+  createWriteStream('products.csv'),  
 )
+
+//por algum motivo, o pipeline async mata a stream na primeira linha do csv, criando dois arrays de jsons dentro de um só arquivo
+asyncPipeline(
+  createReadStream('products.csv', 'utf-8'),
+  toObject,
+  stringify,
+  createWriteStream('arroz.json')
+)
+
+console.log('funcionou')
